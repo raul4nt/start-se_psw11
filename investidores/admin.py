76c23b1f9f1ac73f038e7bcca2baf2eb31658a22
Raul Silveira <raulantsilveira@gmail.com>
2024-09-17 21:04:55 -0300

    add files via upload

---
+++ investidores/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from investidores.models import PropostaInvestimento
+
+admin.site.register(PropostaInvestimento)
---
+++ usuarios/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
+from django.contrib.messages import constants
+
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem.')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha precisa ter pelo menos seis dígitos.')
+            return redirect('/usuarios/cadastro')
+        
+
+        users = User.objects.filter(username=username)
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com este username.')
+            return redirect('/usuarios/cadastro')
+
+
+        user = User.objects.create_user(
+            username=username,
+            password=senha
+        )
+
+        return redirect('/usuarios/logar') 
+
+def logar(request):
+     if request.method == 'GET':   
+        return render(request, 'logar.html')
+     
+     elif request.method == "POST":
+         username = request.POST.get('username')
+         senha = request.POST.get('senha')
+
+         user = auth.authenticate(request, username=username, password=senha)
+         if user:
+             auth.login(request, user)
+             return redirect('/empresarios/cadastrar_empresa')
+         messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos')
+         return redirect('/usuarios/logar')
